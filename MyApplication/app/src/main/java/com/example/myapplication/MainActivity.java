78c8e0fc9a9d87f64b270d80a3781ef4fd2864ec
Maede <maede.d.z@gmail.com>
2019-03-11 01:06:22 +0330

    gysroscope with button

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.myapplication;
-
-
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.BitmapFactory.Options;
+import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -14,6 +13,7 @@ import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.widget.Toast;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.Surface;
@@ -21,8 +21,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
-
-
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
@@ -30,6 +30,8 @@ public class MainActivity extends Activity {
     private SensorManager mSensorManager;
     private WindowManager mWindowManager;
     private Display mDisplay;
+    Button imageButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,9 +39,22 @@ public class MainActivity extends Activity {
         mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         mDisplay = mWindowManager.getDefaultDisplay();
         mSpinner = new Spinner(this);
-        mSpinner.setBackgroundResource(R.drawable.wood);
-        setContentView(mSpinner);
+
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Click on Ball to play!:))", Toast.LENGTH_SHORT).show();
+        addListenerOnButton();
     }
+    public void addListenerOnButton() {
+        imageButton = (Button) findViewById(R.id.imageButtonSelector);
+        imageButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                mSpinner.setBackgroundResource(R.drawable.wood);
+                setContentView(mSpinner);
+            }
+        });
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -54,10 +69,8 @@ public class MainActivity extends Activity {
 
     class Spinner extends FrameLayout implements SensorEventListener {
         private static final float sBallDiameter = 0.004f;
-
         private final int mDstWidth;
         private final int mDstHeight;
-
         private Sensor mGyroscope;
         private long mLastT;
         private float mXDpi;
@@ -70,35 +83,36 @@ public class MainActivity extends Activity {
         private float mSensorY;
         private float mHorizontalBound;
         private float mVerticalBound;
-        private final ParticleSystem mParticleSystem;
+        final ParticleSystem mParticleSystem;
         class Particle extends View {
-            private float mPosX = (float) Math.random();
-            private float mPosY = (float) Math.random();
+            private float mPosX;
+            private float mPosY;
             private float mVelX;
             private float mVelY;
-
             public Particle(Context context) {
                 super(context);
             }
-
             public Particle(Context context, AttributeSet attrs) {
                 super(context, attrs);
             }
-
             public Particle(Context context, AttributeSet attrs, int defStyleAttr) {
                 super(context, attrs, defStyleAttr);
             }
-
             @TargetApi(Build.VERSION_CODES.N)
-
-
             public void computePhysics(float sx, float sy, float dT) {
-                final double ax =  9.81*Math.sin(sy);
-                final double ay = -9.81*Math.sin(sx);
-                mPosX += mVelX * dT + ax * dT * dT / 2;
-                mPosY += mVelY * dT + ay * dT * dT / 2;
-                mVelX += ax * dT;
-                mVelY += ay * dT;
+                double Usx=Math.abs(9.81f*Math.cos(sy)) - Math.abs(9.81f*Math.sin(sy)*0.15f);
+                double Usy = Math.abs(9.81f*Math.cos(sx)) - Math.abs(9.81f*Math.sin(sx)*0.15f);
+                if(Usx >0 && Usy>0){
+                    final int dirx = -(int)(Math.abs(mVelX)/mVelX);
+                    final int diry = -(int)(Math.abs(mVelY)/mVelY);
+                    final double ax =  9.81f*Math.sin(sy) - dirx*9.81f*Math.cos(sy)*0.1f;
+                    final double ay = -9.81f*Math.sin(sx) - diry*9.81f*Math.cos(sx)*0.1f;
+                    mPosX += mVelX * dT + ax * dT * dT / 2;
+                    mPosY += mVelY * dT + ay * dT * dT / 2;
+                    mVelX += ax * dT;
+                    mVelY += ay * dT;
+                }
+
             }
             public void resolveCollisionWithBounds() {
                 final float xmax = mHorizontalBound;
@@ -107,22 +121,20 @@ public class MainActivity extends Activity {
                 final float y = mPosY;
                 if (x > xmax) {
                     mPosX = xmax;
-                    mVelX = 0;
+                    mVelX = -mVelX*0.5f;
                 } else if (x < -xmax) {
                     mPosX = -xmax;
-                    mVelX = 0;
+                    mVelX = -mVelX*0.5f;
                 }
                 if (y > ymax) {
                     mPosY = ymax;
-                    mVelY = 0;
+                    mVelY = -mVelY*0.5f;
                 } else if (y < -ymax) {
                     mPosY = -ymax;
-                    mVelY = 0;
+                    mVelY = -mVelX*0.5f;
                 }
             }
         }
-
-
         class ParticleSystem {
             private Particle mBalls ;
             ParticleSystem() {
@@ -133,12 +145,8 @@ public class MainActivity extends Activity {
                 mBalls.setTranslationX(0);
                 mBalls.setTranslationY(0);
             }
-
-
             private void updatePositions(float sx, float sy, long timestamp) {
                 final long t = timestamp;
-
-
                 if (mLastT != 0) {
                     final float dT = (float) (t - mLastT) / 1000.f;
                     final float dwx = (sx)*dT;
@@ -148,51 +156,37 @@ public class MainActivity extends Activity {
                 }
                 mLastT = t;
             }
-
             public void update(float sx, float sy, long now) {
                 updatePositions(sx, sy, now);
                 Particle curr=mBalls;
                 curr.resolveCollisionWithBounds();
             }
-
-
             public float getPosX() {
                 return mBalls.mPosX;
             }
-
             public float getPosY() {
                 return mBalls.mPosY;
             }
         }
-
         public void startSimulation() {
             mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_GAME);
         }
-
         public void stopSimulation() {
             mSensorManager.unregisterListener(this);
         }
-
         public Spinner(Context context) {
             super(context);
             mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
             DisplayMetrics metrics = new DisplayMetrics();
             getWindowManager().getDefaultDisplay().getMetrics(metrics);
             mXDpi = metrics.xdpi;
             mYDpi = metrics.ydpi;
             mMetersToPixelsX = mXDpi / 0.0254f;
             mMetersToPixelsY = mYDpi / 0.0254f;
-
             mDstWidth = (int) (sBallDiameter * mMetersToPixelsX + 0.5f);
             mDstHeight = (int) (sBallDiameter * mMetersToPixelsY + 0.5f);
             mParticleSystem = new ParticleSystem();
-
-            Options opts = new Options();
-            opts.inDither = true;
-            opts.inPreferredConfig = Bitmap.Config.RGB_565;
         }
-
         @Override
         protected void onSizeChanged(int w, int h, int oldw, int oldh) {
             mXOrigin = (w - mDstWidth) * 0.5f;
@@ -200,8 +194,6 @@ public class MainActivity extends Activity {
             mHorizontalBound = ((w / mMetersToPixelsX - sBallDiameter) * 0.5f);
             mVerticalBound = ((h / mMetersToPixelsY - sBallDiameter) * 0.5f);
         }
-
-
         @Override
         public void onSensorChanged(SensorEvent event) {
             if (event.sensor.getType() != Sensor.TYPE_GYROSCOPE)
